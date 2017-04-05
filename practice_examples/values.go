e783362d3b872d5659eed6c700c52645218637a8
tanviwebonise <tanvi.vanmali@weboniselab.com>
2017-04-05 18:02:20 +0530

    perform operations using using boolean integers

---
+++ practice_examples/values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tanvi"+"Vanmali")
+	fmt.Println("Integers addition:", 1+3)
+	fmt.Println("Floats:", 5.0/0.3)
+	fmt.Println(true && false)
+}
