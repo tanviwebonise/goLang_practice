a454285e6ed56c3ece8125a381b11f71d2d10947
tanviwebonise <tanvi.vanmali@weboniselab.com>
2017-04-10 19:07:32 +0530

    program for function

---
+++ practice_examples/function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func getTotalMarksAndAverage(marks []float64) (float64, float64) {
+	total := 0.0
+	for _, iterator := range marks {
+		total += iterator
+	}
+	return total, (total / float64(len(marks)))
+}
+
+func main() {
+	marks := []float64 {66, 20, 78, 96, 45}
+	total, average := getTotalMarksAndAverage(marks)
+	fmt.Println("Total marks:", total, "average:", average)
+}
